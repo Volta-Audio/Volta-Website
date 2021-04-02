6282902526ed40dc5f5b493ca5fa9e6b30a341e8
Abigail Coe <abigailcoe@gmail.com>
2021-04-02 16:36:36 +0100

    add svg plugin

--- static.config.js
+++ static.config.js
@@ -28,6 +28,7 @@ export default {
   plugins: [
     "react-static-plugin-typescript",
     "react-static-plugin-styled-components",
+    "react-static-plugin-svg",
     [
       require.resolve("react-static-plugin-source-filesystem"),
       {
